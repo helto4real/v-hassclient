7fca3bc3e374a9894218dccc9fca35b41c821cc5
helto4real <tomas.hellstrom@yahoo.se>
2021-06-16 11:16:32 +0200

    fmt

--- main.v
+++ main.v
@@ -1,27 +1,23 @@
 module main
+
 import hassclient as hc
 import log
 import os
 
 fn main() {
-	mut c := hc.new_connection(
-		hc.ConnectionConfig {
-			hass_uri: "ws://192.168.1.20:8124/api/websocket",
-			token: '' // Uses the HASS_TOKEN env instead if empty
-			log_level: log.Level.debug
-		}
-	) or {panic(err)}
+	mut c := hc.new_connection(hc.ConnectionConfig{
+		hass_uri: 'ws://192.168.1.20:8124/api/websocket'
+		token: '' // Uses the HASS_TOKEN env instead if empty
+		log_level: log.Level.debug
+	}) or { panic(err) }
 	go c.connect()
 	go handle_new_messages(mut c)
 	os.get_line()
-	
 }
 
-fn handle_new_messages(mut c hc.HassConnection) 
-{
+fn handle_new_messages(mut c hc.HassConnection) {
 	for {
-		mut event_data :=  <- c.ch_state_changed  
+		mut event_data := <-c.ch_state_changed
 		println('FROM CHANNEL: $event_data')
-
 	}
 }
