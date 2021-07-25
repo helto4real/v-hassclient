34c6597759d49542459830fdc9a48dc7ee77cadf
helto4real <tomas.hellstrom@yahoo.se>
2021-07-25 23:38:16 +0200

    Comments

--- hassclient/api.v
+++ hassclient/api.v
@@ -2,6 +2,7 @@ module hassclient
 
 import x.json2
 
+// call_service makes a service call to the given domain and service using entities as target
 pub fn (mut c HassConnection) call_service(domain string, service string, service_data json2.Any, entity_id ...string) ? {
 	c.sequence++
 
@@ -12,6 +13,7 @@ pub fn (mut c HassConnection) call_service(domain string, service string, servic
 	c.ws.write_string(str_encoded.str()) ?
 }
 
+// call_service_with_device makes a service call to the given domain and service using devices as target
 pub fn (mut c HassConnection) call_service_with_device(domain string, service string, service_data json2.Any, device_id ...string) ? {
 	c.sequence++
 
@@ -22,6 +24,7 @@ pub fn (mut c HassConnection) call_service_with_device(domain string, service st
 	c.ws.write_string(str_encoded.str()) ?
 }
 
+// call_service_with_area makes a service call to the given domain and service using areas as target
 pub fn (mut c HassConnection) call_service_with_area(domain string, service string, service_data json2.Any, area_id ...string) ? {
 	c.sequence++
 
